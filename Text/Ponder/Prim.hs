262399f566ac8c59d337ef24bbbe86b525408bbf
azure <azure4625@gmail.com>
2014-03-10 15:11:07 +0900

    Add option

--- Text/Ponder/Prim.hs
+++ Text/Ponder/Prim.hs
@@ -2,13 +2,18 @@ module Text.Ponder.Prim
 ( ParserT(..)
 , andP
 , notP
+, option
 ) where
 
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.Error
+import Control.Applicative
 
 type ParserT s e m a = StateT s (ErrorT e m) a
+
+option :: (Monad m, Functor m, Error e) => a -> ParserT s e m a -> ParserT s e m a
+option a p = p <|> return a
 
 andP :: Monad m => StateT s m a -> StateT s m ()
 andP p = StateT $ \s -> (runStateT p s) >> return ((), s)
